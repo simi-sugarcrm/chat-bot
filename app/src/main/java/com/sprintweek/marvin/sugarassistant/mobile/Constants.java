76cef6b29237082975ca59e508dc0a02c71f85e6
Simi Fagbemi <simi.fagbemi@sugarcrm.com>
2020-09-30 14:34:31 -0400

    Finished tutorial

---
+++ app/src/main/java/com/sprintweek/marvin/sugarassistant/mobile/Constants.java
@@ -0,0 +1,8 @@
+package com.sprintweek.marvin.sugarassistant.mobile;
+
+public class Constants {
+    static int HUMAN_ID = 0;
+    static int AI_ID = 1;
+    static int VIEW_TYPE_MESSAGE_SENT = 2;
+    static int VIEW_TYPE_MESSAGE_RECEIVED = 3;
+}
--- app/src/main/java/com/sprintweek/marvin/sugarassistant/mobile/MessageListActivity.java
+++ app/src/main/java/com/sprintweek/marvin/sugarassistant/mobile/MessageListActivity.java
@@ -1,6 +1,23 @@
 package com.sprintweek.marvin.sugarassistant.mobile;
 
-public class MessageListActivity {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MessageListActivity extends AppCompatActivity {
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_list);
+
+        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        mMessageAdapter = new MessageListAdapter(this, new ArrayList());
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
+    }
 }
-
