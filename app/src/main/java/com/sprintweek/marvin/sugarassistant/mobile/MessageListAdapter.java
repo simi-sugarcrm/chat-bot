2978dccbe4bc11d5a123dc9392e05f1e563590b2
Simi Fagbemi <simi.fagbemi@sugarcrm.com>
2020-10-01 00:54:48 -0400

    Added greeting and fixed other things

--- app/src/main/java/com/sprintweek/marvin/sugarassistant/mobile/MessageListAdapter.java
+++ app/src/main/java/com/sprintweek/marvin/sugarassistant/mobile/MessageListAdapter.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -28,12 +27,12 @@ public class MessageListAdapter extends RecyclerView.Adapter {
             profileImage = (ImageView) itemView.findViewById(R.id.image_message_profile);
         }
 
-        void bind(UserMessage message) {
+        void bind(EntityMessage message) {
             messageText.setText(message.getMessage());
 
             // Format the stored timestamp into a readable String using method.
-            timeText.setText(Long.toString(message.getCreatedAt()));
-            nameText.setText(message.getSender().getNickname());
+            timeText.setText(message.getFormattedCreatedAt());
+            nameText.setText(message.getSender().getName());
 
             // Insert the profile image from the URL into the ImageView.
             //Utils.displayRoundImageFromUrl(mContext, message.getSender().getProfileUrl(), profileImage);
@@ -49,7 +48,7 @@ public class MessageListAdapter extends RecyclerView.Adapter {
             timeText = (TextView) itemView.findViewById(R.id.text_message_time);
         }
 
-        void bind(UserMessage message) {
+        void bind(EntityMessage message) {
             messageText.setText(message.getMessage());
 
             // Format the stored timestamp into a readable String using method.
@@ -72,9 +71,9 @@ public class MessageListAdapter extends RecyclerView.Adapter {
     // Determines the appropriate ViewType according to the sender of the message.
     @Override
     public int getItemViewType(int position) {
-        UserMessage message = (UserMessage) mMessageList.get(position);
+        EntityMessage message = (EntityMessage) mMessageList.get(position);
 
-        if (message.getSender().getUserId() != Constants.AI_ID) {
+        if (message.getSender().getEntityId() != Constants.AI_ID) {
             // If the current user is the sender of the message
             return Constants.VIEW_TYPE_MESSAGE_SENT;
         } else {
@@ -104,7 +103,7 @@ public class MessageListAdapter extends RecyclerView.Adapter {
     // Passes the message object to a ViewHolder so that the contents can be bound to UI.
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        UserMessage message = (UserMessage) mMessageList.get(position);
+        EntityMessage message = (EntityMessage) mMessageList.get(position);
         if (holder.getItemViewType() == Constants.VIEW_TYPE_MESSAGE_SENT){
             ((SentMessageHolder) holder).bind(message);
         }
--- app/src/main/java/com/sprintweek/marvin/sugarassistant/mobile/User.java
+++ app/src/main/java/com/sprintweek/marvin/sugarassistant/mobile/User.java
@@ -1,31 +1,26 @@
 package com.sprintweek.marvin.sugarassistant.mobile;
 
-public class User {
-    boolean human;
-    String nickname;
+public class User extends Entity{
+
+    String name;
     String profileUrl;
 
-    public User(boolean human, String nickname, String profileUrl) {
-        this.human = human;
-        this.nickname = nickname;
+    public User(String name, String profileUrl){
+        this.name = name;
         this.profileUrl = profileUrl;
     }
 
+    @Override
+    public int getEntityId() {
+        return Constants.HUMAN_ID;
+    }
 
-    public String getNickname(){
-        return this.nickname;
+    @Override
+    public String getName(){
+        return this.name;
     }
 
     public String getProfileUrl(){
         return this.profileUrl;
-    }
-
-    public int getUserId(){
-        if (human){
-            return Constants.HUMAN_ID;
-        }
-        else{
-            return Constants.AI_ID;
-        }
     }
 }
